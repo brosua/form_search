aefede4c4b4d466d3d74fd50afb57d57dcf53bb4
brosua <j.vogel97@web.de>
2022-04-18 21:38:20 +0200

    [TASK] Add version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,20 @@
 <?php
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Form search backend',
     'description' => 'Add search functionality to the TYPO3 backend form manager',
-    'category' => 'misc',
+    'category' => 'be',
     'author' => 'Josua Vogel',
     'author_email' => 'j.vogel97@web.de',
     'state' => 'beta',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
     'constraints' => [
-        'depends' => ['typo3' => '11.5.0-11.99.99'],
-        'conflicts' => [],
-        'suggests' => []
-    ]
+        'depends' => [
+            'typo3' => '11.5.0-11.99.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
 ];
