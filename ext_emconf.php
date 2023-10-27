d09b99daff599e2e48d934a4d2853439b3417862
Achim Fritz <achim.fritz@b13.de>
2023-10-27 11:08:21 +0200

    [TASK] add TYPO3 12 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,5 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Form search backend',
     'description' => 'Add search functionality to the TYPO3 backend form manager',
@@ -10,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.99.99',
+            'typo3' => '11.5.0-12.99.99',
         ],
         'conflicts' => [
         ],
